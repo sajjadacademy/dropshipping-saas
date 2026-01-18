034ad12804758ea8c749dec419192567c0e8810e
Sajjad Academy <60256388+sajjadacademy@users.noreply.github.com>
2026-01-18 16:08:00 +0400

    ix sign-in page prerender

--- frontend/app/sign-in/page.tsx
+++ frontend/app/sign-in/page.tsx
@@ -1,5 +1,7 @@
 import { SignIn } from "@clerk/nextjs";
 
+export const dynamic = 'force-dynamic';
+
 export default function Page() {
     return (
         <div className="flex justify-center items-center min-h-screen bg-deep-obsidian">
